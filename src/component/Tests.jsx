4409182733b243baf3e2a20bf211ee502889396c
faroukq1 <faroukq1@gmail.com>
2024-01-04 14:10:42 +0100

    making the carrosel

--- src/component/Tests.jsx
+++ src/component/Tests.jsx
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 import { useGlobalContext } from '../context/GlobalContext';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 const Tests = () => {
   const { exam } = useGlobalContext();
+  const [currentItem, setCurrentItem] = useState(0);
   const examQestionList = exam[0]?.question || [];
-  const id = exam.id;
+  console.log(currentItem);
   return (
     <Wrapper>
       {examQestionList.map((exam) => {
         const { question, answers } = exam;
         return (
-          <div className="question-container" key={nanoid()}>
+          <div
+            className="question-container"
+            style={{ transform: `translateY(${-currentItem * 105}%)` }}
+            key={nanoid()}
+          >
             <h1>{question}</h1>
             <div className="question">
               {answers.map((ans) => (
@@ -21,23 +27,48 @@ const Tests = () => {
           </div>
         );
       })}
+      <div className="change-btn">
+        <button
+          onClick={() =>
+            setCurrentItem((prev) =>
+              prev === 0
+                ? (prev = examQestionList.length - 1)
+                : (prev - 1) % examQestionList.length
+            )
+          }
+        >
+          Back
+        </button>
+        <button
+          onClick={() =>
+            setCurrentItem((prev) => (prev + 1) % examQestionList.length)
+          }
+        >
+          Next
+        </button>
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.article`
-  height: 100%;
-  width: 100%;
   background-color: #1c1b25;
+  overflow: hidden;
+  transition: 1ms all ease;
+  position: relative;
   .question-container {
-    padding: 4rem;
+    padding: 3rem;
+    background-color: #4e000032;
+    margin: 2rem auto 0 auto;
     display: flex;
     align-items: center;
     flex-direction: column;
     gap: 2rem;
-    height: 100%;
-    width: 100%;
+    height: 90%;
+    width: 70%;
+    border-radius: 1rem;
     h1 {
+      margin: 2rem 0 0 0;
       color: white;
       text-align: center;
       font-size: 30px;
@@ -47,10 +78,26 @@ const Wrapper = styled.article`
       width: 100%;
       display: flex;
       justify-content: space-between;
+      gap: 3rem;
       p {
         color: white;
         font-weight: bold;
       }
+    }
+  }
+  .change-btn {
+    position: absolute;
+    bottom: 20%;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    button {
+      padding: 0.5rem 2rem;
+      outline: none;
+      border-color: transparent;
+      background-color: white;
+      border-radius: 10px;
+      cursor: pointer;
     }
   }
 `;
